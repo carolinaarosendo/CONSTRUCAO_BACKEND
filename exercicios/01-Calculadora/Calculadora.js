f4e45a61f63a8538aaa7d19c41026f1ac4545da9
carolinaarosendo <carolinaarosendo@gmail.com>
2025-08-25 20:15:52 -0300

    Exercício Prático: Calculadora em Node

---
+++ exercicios/01-Calculadora/Calculadora.js
@@ -0,0 +1,32 @@
+function soma(a,b){
+    return a + b
+}
+
+function subtrair(a,b){
+    return a - b
+}
+
+function multiplicar(a,b){
+    return a * b
+}
+
+function divisao(a,b){
+    return a / b
+}
+
+function aoQuadrado(a,b){
+    return a ** 2
+}
+
+function raizQuadrada(a){
+    return Math.sqrt(a)
+}
+
+module.exports = {
+    soma,
+    subtrair,
+    multiplicar,
+    divisao,
+    aoQuadrado,
+    raizQuadrada
+}
---
+++ exercicios/01-Calculadora/index.js
@@ -0,0 +1,8 @@
+const calc = require('./Calculadora');
+console.log("### Exercício Prático: Calculadora em Node ###")
+console.log('a) Soma: 3 + 4 =', calc.soma(3, 4));
+console.log('b) Subtração: 3 - 4 =',calc.subtrair(3,4));
+console.log('c) Multiplicação: 3 * 4 =', calc.multiplicar(3,4));
+console.log('d) Divisão: 3 / 4 =', calc.divisao(3,4));
+console.log('e) 3 Ao Quadrado: =', calc.aoQuadrado(3,2));
+console.log('f) Raiz Quadrada de 25:', calc.raizQuadrada(25));
